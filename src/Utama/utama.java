e5ac288bb2b33db786727b8946cb3219ab7a3c80
ArieDZ_2 <ArieDZ_2@DESKTOP-HNOOKEG>
2017-10-18 23:44:12 +0700

    BASE

---
+++ src/Utama/utama.java
@@ -0,0 +1,20 @@
+package Utama;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ArieDZ_2
+ */
+public class utama {
+    public static void main (String[] args) throws ClassNotFoundException
+    {
+        frm_Utama_metro frmUtama = new frm_Utama_metro();
+        frmUtama.setVisible(true);
+    }
+    
+}
